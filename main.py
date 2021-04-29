cf626268e0717f1db2a91348fcdc6298c20cbb9e
arampatzisgiorgos <48099515+arampatzisgiorgos@users.noreply.github.com>
2021-04-29 21:19:06 +0300

    20210429 - Comments - v1.3

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from read_data import *
 from scraper import *
+from directories import *
 
 if __name__ == '__main__':
+    """
+    you need a:
+    ----> data/history/ <----- 
+    directory with:
+     influencer1_name.csv
+     influencer2_name.csv
+     ...
+     influencerN_name.csv
+    """
     # get all influencers' names
     account_names = get_influencers_names()
     # for all of them get the URL posts
