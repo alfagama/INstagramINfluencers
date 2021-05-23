bdc75ca3dfa7889b11a46c1eaa43f435649bcb40
alexiafy <alexia.fytili@gmail.com>
2021-05-23 15:14:24 +0300

    20210523 - Web app - 1.2

--- web_app/app.py
+++ web_app/app.py
@@ -1,11 +1,15 @@
 from flask import Flask  # Flask -> class name
-from flask import render_template
+from flask import render_template, request
 from flask import jsonify
 import os
 from web_app.funcs.db import Db
+import plotly
+import plotly.express as px
+import pandas as pd
+import json
 
 # declare application. initialize with Flask instance/class
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='static/stylesheets')
 
 # get MongoDB instance
 db = Db()
@@ -13,10 +17,13 @@ db = Db()
 
 # declare route # can also declare methods to accept. like -> 'GET'
 # @app.route("/", methods=['GET'])
-@app.route("/")
-def home():
-    todos = db.find_all('todos')
-    return render_template('home.html', todos=list(todos))
+@app.route("/static/stylesheets/home.html")
+def index():
+    influencer_count_by_category_df = db.find_influencers_count_by_category()
+    #fig = px.bar(influencer_count_by_category_df, x='_id', y='count', barmode='group')
+    fig = px.pie(influencer_count_by_category_df, values='count', names='_id', title='Percentage of influencers from each category')
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template("home.html", graph=graphJSON)
 
 
 @app.route("/todo/<importance_val>")
--- web_app/funcs/db.py
+++ web_app/funcs/db.py
@@ -1,4 +1,17 @@
 import pymongo
+import pandas as pd
+
+
+# MongoDB
+uri = "mongodb://localhost:27017/"
+# Mongo client
+client = pymongo.MongoClient(uri)
+# Import database by name
+db = client.ININ
+# Get collection from DB
+CollectionName = 'myLeaderboards'
+# set collection
+collection = db[CollectionName]
 
 
 class Db:
@@ -6,8 +19,21 @@ class Db:
         client = pymongo.MongoClient('localhost', 27017)
         self.db = client['todo-db']
 
+
     def find_all(self, collection_name: str):
         return list(self.db[collection_name].find({}, {'_id': False}))
 
-    def find(self, collection_name:str, query:dict):
+
+    def find(self, collection_name: str, query:dict):
         return list(self.db[collection_name].find(query, {'_id': False}))
+
+
+    def find_influencers_count_by_category(self):
+        cursor = collection.aggregate([
+            {'$group': {'_id': '$category', "count": {"$sum": 1}}}
+        ])
+        return pd.DataFrame(list(cursor))
+
+
+
+
