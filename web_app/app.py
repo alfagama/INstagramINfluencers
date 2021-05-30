2241718f416f3b9ce112eaae6e37e0b9ab198158
alexiafy <alexia.fytili@gmail.com>
2021-05-30 16:04:17 +0300

    20210530 - Web app - 1.1

--- web_app/app.py
+++ web_app/app.py
@@ -7,6 +7,8 @@ import plotly
 import plotly.express as px
 import pandas as pd
 import json
+import collections
+
 
 # declare application. initialize with Flask instance/class
 app = Flask(__name__, template_folder='static/stylesheets')
@@ -18,6 +20,8 @@ db = Db()
 @app.route('/wordclouds')
 def word_loud():
     return render_template('wordclouds.html')
+
+
 # declare route # can also declare methods to accept. like -> 'GET'
 @app.route("/", methods=['GET'])
 @app.route("/static/stylesheets/home.html")
@@ -29,12 +33,48 @@ def index():
     return render_template("home.html", graph=graphJSON)
 
 
+@app.route("/static/stylesheets/statistics.html")
+def statistics():
+    '''if request.method == "POST":
+        app.logger.warning('dfsdfdsfsdfsd')
+        category = request.form["category"]
+        app.logger.warning(category)'''
+    df = db.get_date_frequency()
+    #fig = px.line(df, x="date", y="frequency", title='Posts frequency by date')
+    fig = px.bar(df, x="date", y="frequency", title="Posts frequency by date")
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template("statistics.html", graph=graphJSON)
+
+
+@app.route("/static/stylesheets/clustering.html")
+def clustering():
+    df = db.get_post_hashtags()
+
+    all_hashtags = []
+    for hashtag_list in df['hashtags']:
+        for influencer in hashtag_list:
+            for post in influencer:
+                all_hashtags.append(post)
+
+    print(all_hashtags)
+    posts_hashtag_count = []
+
+    for list in all_hashtags:
+        posts_hashtag_count.append(len(list))
+
+    counter = collections.Counter(posts_hashtag_count)
+    counter_df = pd.DataFrame.from_dict(counter, orient='index').reset_index()
+    counter_df = counter_df.rename(columns={'index': 'Number of Hashtags', 0: 'Number of Posts'})
+
+    fig = px.bar(counter_df, x="Number of Hashtags", y="Number of Posts", title="Hashtags Distribution")
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return render_template("clustering.html", number_of_hashtags=graphJSON)
+
+
 @app.route("/todo/<importance_val>")
 def get_todo(importance_val):
     todos = db.find('todos', {"importance": int(importance_val)})
     return jsonify(todos)
-
-
 
 
 if __name__ == "__main__":
--- web_app/funcs/db.py
+++ web_app/funcs/db.py
@@ -1,6 +1,7 @@
-import pandas as pd
 import pymongo
-from pandas import DataFrame
+import pandas as pd
+import collections
+
 
 # MongoDB
 uri = "mongodb://localhost:27017/"
@@ -9,7 +10,7 @@ client = pymongo.MongoClient(uri)
 # Import database by name
 db = client.ININ
 # Get collection from DB
-CollectionName = 'myLeaderboards'
+CollectionName = 'myLeaderboardsNew'
 # set collection
 collection = db[CollectionName]
 
@@ -19,11 +20,15 @@ class Db:
         client = pymongo.MongoClient('localhost', 27017)
         self.db = client['todo-db']
 
+
     def find_all(self, collection_name: str):
         return list(self.db[collection_name].find({}, {'_id': False}))
 
-    def find(self, collection_name: str, query: dict):
+
+    def find(self, collection_name: str, query:dict):
         return list(self.db[collection_name].find(query, {'_id': False}))
+
+
 
     def find_influencers_count_by_category(self):
         cursor = collection.aggregate([
@@ -31,7 +36,39 @@ class Db:
         ])
         return pd.DataFrame(list(cursor))
 
-    def get_post_comments(self):
-        comments = collection.find()
-        df = DataFrame(list(comments))
+
+    def get_date_frequency(self):
+        cursor = collection.find({}, {'_id': 0, 'Posts.Post Created Date': 1})
+
+        post_list = []
+        for element in cursor:
+            post_list.append(element['Posts'])
+
+        all_dates = []
+        for posts in post_list:  # for each influencer
+            for post in posts:  # for each post of each influencer
+                if post.get('Post Created Date') != '2021-04-30':  # filter out this date because we have only 3 posts
+                    all_dates.append(post.get('Post Created Date'))
+
+        counter = collections.Counter(all_dates)  # count frequency of each date
+        df = pd.DataFrame.from_dict(counter, orient='index').reset_index()  # convert conter to df
+        df.columns = ['date', 'frequency']
         return df
+
+
+    def get_post_hashtags(self):
+        """
+        Gets a dataframe with the description of posts grouped by influencer's category
+        :param: -
+        :return posts_description_df: dataframe with posts' description
+        """
+
+        cursor = collection.aggregate([
+            {'$group': {'_id': '$category',
+                    'hashtags': {'$push': '$Posts.Hashtags'}}}
+        ])
+
+        posts_description_df = pd.DataFrame(list(cursor))
+        posts_description_df.columns = ['category', 'hashtags']
+        return posts_description_df
+
