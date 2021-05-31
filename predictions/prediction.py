280e97dd074e1c1fa0b92ba18b4d74b993261625
elenitsiolaki <tsiolakie@gmail.com>
2021-05-31 15:23:26 +0300

    20210531 - prediction - v1.20.4

--- predictions/prediction.py
+++ predictions/prediction.py
@@ -10,7 +10,7 @@ from sklearn.svm import LinearSVC, SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import BaggingClassifier, AdaBoostClassifier, RandomForestClassifier, VotingClassifier
 from matplotlib import pyplot
-from dataset_creation import read_data
+#from dataset_creation import read_data
 from pandas.api.types import is_string_dtype
 from sklearn.decomposition import PCA
 import numpy as np
@@ -21,7 +21,6 @@ import matplotlib.pyplot as plt
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
-# Models' scores
 accuracy = []
 precision = []
 recall = []
@@ -45,8 +44,8 @@ def read_data_from_questionnaire():
     print(dataset.head(5))
 
     print(dataset.columns)
-    #Drop name and reasons columns
-    dataset.drop(['name', 'reasons'], axis='columns', inplace=True)
+    #Drop name, reasons & category columns
+    dataset.drop(['name', 'reasons', 'category'], axis='columns', inplace=True)
     print(dataset.shape)
 
     #Convert all string values to numeric
@@ -102,7 +101,7 @@ def print_scores(y_true, y_pred, model_name):
     f1.append(metrics.f1_score(y_true, y_pred))
     model_names.append(model_name)
 
-def plot_results():
+def plot_results(name):
 
     x = np.arange(len(model_names))
     width = 0.2  # the width of the bars
@@ -113,13 +112,17 @@ def plot_results():
     plt.bar(X_axis + 0.1, recall, width, label='Recall')
     plt.bar(X_axis + 0.3, f1, width, label='F1')
 
-    plt.xticks(X_axis, model_names)
+    plt.xticks(X_axis, model_names, rotation=45)
+    # Pad margins so that markers don't get clipped by the axes
+    plt.margins(0.2)
+    # Tweak spacing to prevent clipping of tick-labels
+    plt.subplots_adjust(bottom=0.25)
     plt.xlabel("Models")
     plt.ylabel("Scores")
     plt.title("Machine Learning Model Scores")
     plt.legend()
+    plt.savefig(name + '.png')
     plt.show()
-    plt.savefig('prediction_on_mongo_info.png')
 
 
 def fit_predict(x_train, x_test, model):
@@ -146,28 +149,29 @@ def make_prediction(dataset, prediction_type):
         pca.fit(x_train_s)
         x_train_final = pca.transform(x_train_s)
         x_test_final = pca.transform(x_test_s)
+    else:
+        x_train_final = x_train_s
+        x_test_final = x_test_s
 
 
-    """
-    # Naive Bayes
-    # ---------------------------------------------------------------
-    mnb = MultinomialNB().fit(x_train, y_train)
+    if prediction_type == 'questionnaire_info':
+        # Naive Bayes
+        # ---------------------------------------------------------------
+        mnb = MultinomialNB().fit(x_train, y_train)
+        y_predicted = mnb.predict(x_test_final)
+        print_scores(y_test, y_predicted, "MultinomialNB")
+        print("score on train: ", str(mnb.score(x_train_final, y_train)), "\n")
+        # ---------------------------------------------------------------
 
-    #y_k_means_predicted = fit_predict(x_train_final, y_train, mnb)
-    labels = mnb.predict(x_test_final)
-    
-    print_scores(y_test, labels, "MultinomialNB")
-    print("score on train: ", mnb(model.score(x_train_final, y_train)), "\n")
-    """
 
     # Logistic Regression
     # ---------------------------------------------------------------
     grid = {"C": np.logspace(-3, 3, 7), "tol": [1e-2, 1e-3, 1e-4, 1e-5], "penalty": ["l1", "l2"], "solver": ["saga"], "max_iter": [5000]}
-    model = GridSearchCV(LogisticRegression(), param_grid=grid)
-    model.fit(x_train_final, y_train)
-    y_predicted = model.predict(x_test_final)
+    lr = GridSearchCV(LogisticRegression(), param_grid=grid)
+    lr.fit(x_train_final, y_train)
+    y_predicted = lr.predict(x_test_final)
     print_scores(y_test, y_predicted, "Logistic Regression")
-    print("score on train: ", str(model.score(x_train_final, y_train)), "\n")
+    print("score on train: ", str(lr.score(x_train_final, y_train)), "\n")
     # ---------------------------------------------------------------
 
 
@@ -250,33 +254,43 @@ def make_prediction(dataset, prediction_type):
     pyplot.show()
     # ---------------------------------------------------------------
 
-    """
-    # Voting Classifier
-    # ---------------------------------------------------------------
-    # 1) naive bias = mnb
-    # 2) logistic regression =lr
-    # 3) random forest =rf
-    # 4) support vector machine = svm
-    evc = VotingClassifier(estimators=[('mnb', mnb), ('lr', lr), ('rf', rf), ('svm', svm)], voting='hard')
-    evc.fit(x_train, y_train)
-    print("score on test: " + str(evc.score(x_test, y_test)))
-    print("score on train: " + str(evc.score(x_train, y_train)))
-    print()
-    # ---------------------------------------------------------------
-    """
+    if prediction_type == 'questionnaire_info':
+        # Voting Classifier
+        # ---------------------------------------------------------------
+        # 1) naive bias = mnb
+        # 2) logistic regression =lr
+        # 3) random forest =rf
+        # 4) support vector machine = svm
+        evc = VotingClassifier(estimators=[('mnb', mnb), ('lr', lr), ('rf', rf), ('svm', svm)], voting='hard')
+        evc.fit(x_train_final, y_train)
+        y_predicted = evc.predict(x_test_final)
+        print_scores(y_test, y_predicted, "Voting Classifier")
+        print("score on train: " + str(evc.score(x_train_final, y_train)), "\n")
+        # ---------------------------------------------------------------
 
 
 if __name__ == '__main__':
 
     #Make prediction on the questionnaire
-    #dataset_quest = read_data_from_questionnaire()
-    #make_prediction(dataset_quest,'questionnaire')
+    print("Machine learning methods: Questionnaire")
+    dataset_quest = read_data_from_questionnaire()
+    make_prediction(dataset_quest,'questionnaire_info')
+    plot_results("questionnaire")
 
-    # Make prediction on the crowtangle's info
-    dataset_mongo = read_data.get_influencer_info()
-    dataset_mongo_updated = preprocess_data_from_mongo(dataset_mongo)
-    dataset_mongo_updated.to_csv('prediction_info.csv', index=False)
-    make_prediction(dataset_mongo_updated, 'mongo_info')
-    #df = pd.read_csv('prediction_info.csv')
-    #make_prediction(df, 'mongo_info')
-    plot_results()
+    accuracy = []
+    precision = []
+    recall = []
+    f1 = []
+    model_names = []
+
+    # Make prediction on the crowdtangle's info
+    print("Machine learning methods: Crowdtangle")
+    #dataset_mongo = read_data.get_influencer_info()
+    #dataset_mongo_updated = preprocess_data_from_mongo(dataset_mongo)
+    #dataset_mongo_updated.to_csv('prediction_info.csv', index=False)
+    #make_prediction(dataset_mongo_updated, 'mongo_info')
+    df = pd.read_csv('prediction_info.csv')
+    make_prediction(df, 'mongo_info')
+    plot_results("crowdtangle")
+    
+
